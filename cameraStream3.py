5171d22e98a0aeb98650ce7132825d7a940dc0ed
Keith Ellis <keith.ellis@mac.com>
2016-01-31 22:00:27 +0000

    cameraStream3 now updated to record for 30 minutes, then stop and imediately restart

--- cameraStream3.py
+++ cameraStream3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import subprocess
 import picamera
 import time
@@ -10,33 +11,54 @@ STREAM_KEY="fKb9NJUycD2unefr9JhukXybZBRSB3Wq"
 
 
 # ffmpeg command line
-cmdline =['ffmpeg', '-i' , '-', '-vcodec', 'copy', '-an', '-metadata', 'title=\"Streaming from raspberry pi camera\"', '-f', 'flv', RTMP_URL +'/' + STREAM_KEY]
-
+cmdline =['ffmpeg', '-i' , '-', '-vcodec', 'copy', '-an', '-framerate', '25','-metadata', 'title=\"Ipswich IP1 bird box camera\"', '-f', 'flv', RTMP_URL +'/' + STREAM_KEY]
 
 # Setup up pipe to ffmpeg
 stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
-
+#Set up picamera
 camera = picamera.PiCamera()
+
+count = 0
+
+# Function to determine if camera is recording
+# If recording return False, is not recording return True
+def not_recording():
+	if camera.recording:
+		return False
+	else:
+		print("re-starting recording")
+		return True
+		
+#Function to start streaming for 30 minutes and then stop
+def stream_now():
+	print("About to start recording")
+	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
+	print("Record for 30 minutes")
+	camera.wait_recording(1800)
+	print("15 seconds expired, about to stop")
+	camera.stop_recording()
+
+#Main code
 try:
-	camera.resolution = (640, 480)
-	camera.framerate = 30
+	camera.resolution = (960, 540)
+	camera.framerate = 25
 	camera.vflip = True
 	camera.hflip = True
-	print("Starting video streaming")
-	camera.start_recording(stream.stdin, format='h264', quality = 30)
-	print("waiting 30 seconds")
-	try:
-		while True:
-			pass
-	except KeyboardInterrupt:
+#Main loop
+# if not recording, start recording for 15 seconds
+	while True:
+		if not_recording():
+			count += 1
+			stream_now()
+
+except KeyboardInterrupt:
 		camera.stop_recording()
-	#camera.wait_recording(30)
-	print("30 seconds expired, stopping stream")
-#	camera.stop_recording()
+
 finally:
 	camera.close()
 	stream.stdin.close()
 	stream.wait()
-
-
+	print("Camera safely shut down")
+	print("Good bye")
+	print("Count = ",count)

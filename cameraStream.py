d5572d6b245d4b2c7634c8b8c55c0fc821eb38a2
Keith Ellis <keith.ellis@mac.com>
2016-02-02 23:03:56 +0000

    cameraStream.py modified to take snap shots at regular intervals The resolution is as per the recording resolution.

--- cameraStream.py
+++ cameraStream.py
@@ -5,6 +5,9 @@
 # Raspberry Pi camera to uStream using picamera piped to ffmpeg
 # internally within this python script using subprocess.Popen
 # ---------------------------------------------------------------------
+# Added snap_shot()
+# this takes a snapshot every 10 seconds
+
 
 import subprocess
 import picamera
@@ -16,6 +19,10 @@ import time
 RTMP_URL="rtmp://1.21705518.fme.ustream.tv/ustreamVideo/21705518"
 STREAM_KEY="fKb9NJUycD2unefr9JhukXybZBRSB3Wq"
 
+count = 0  #counts how many loops and also used to name the image files
+snapshot_frequency = 10  #frequency of snapshops in seconds
+
+
 
 # ffmpeg command line
 cmdline =['ffmpeg', '-i' , '-', '-vcodec', 'copy', '-an', '-framerate', '25','-metadata', 'title=\"Ipswich IP1 bird box camera\"', '-f', 'flv', RTMP_URL +'/' + STREAM_KEY]
@@ -26,7 +33,6 @@ stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 #Set up picamera
 camera = picamera.PiCamera()
 
-count = 0
 
 # Function to determine if camera is recording
 # If recording return False, is not recording return True
@@ -42,22 +48,29 @@ def stream_now():
 	print("About to start recording")
 	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
 	print("Record for 30 minutes")
-	camera.wait_recording(1800)
+	camera.wait_recording(snapshot_frequency)
 	print("30 minutes expired, about to stop")
 	camera.stop_recording()
+
+#Function to take a snapshop image	
+def snap_shot(name):
+	camera.capture('images/'+ name +'.jpg', use_video_port=True)    
+
 
 #Main code
 try:
 	camera.resolution = (960, 540)
-	camera.framerate = 25
+	camera.framerate = 30
 	camera.vflip = True
 	camera.hflip = True
-#Main loop
-# if not recording, start recording for 15 seconds
+	
+# Main loop if not streaming, start take snapstop and stream again
 	while True:
 		if not_recording():
 			count += 1
 			stream_now()
+		print("taking snap shot")
+		snap_shot('image'+str(count))
 
 except KeyboardInterrupt:
 		camera.stop_recording()

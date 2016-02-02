e1f50d4348d3314a2ca3a6b61ac090a37b5f8333
Keith Ellis <keith.ellis@mac.com>
2016-02-02 20:54:57 +0000

    General cleanup with comments added and some old files removed

--- cameraStream.py
+++ cameraStream.py
@@ -1,188 +1,71 @@
 #!/usr/bin/env python3
 
-# --------------------------------------------------------------------
-# This is a sample python script which streams picamera output to stout
-# it is intended to be called with a script, similar to 
-# startPythonStream.sh.
-# The source of this is from Instructables.com
-# http://www.instructables.com/id/Raspberry-Pi-Birdbox-Camera/?ALLSTEPS
+# ---------------------------------------------------------------------
+# Basic implimentation of fully python implimentation of streaming
+# Raspberry Pi camera to uStream using picamera piped to ffmpeg
+# internally within this python script using subprocess.Popen
 # ---------------------------------------------------------------------
 
-#import smtplib
-#import email
-import mimetypes
-#import StringIO
 import subprocess
-import os
-import time
-import sys
-import time
 import picamera
-from io import StringIO
-
-from datetime import datetime
-
-#from PIL import Image
-#from email.MIMEMultipart import MIMEMultipart
-#from email.Utils import COMMASPACE
-#from email.MIMEBase import MIMEBase
-#from email.parser import Parser
-#from email.MIMEImage import MIMEImage
-#from email.MIMEText import MIMEText
-#from email.MIMEAudio import MIMEAudio</p><p># Original code written by brainflakes and modified to exit
-
-#image scanning for loop as soon as the sensitivity value is exceeded.
-# this can speed taking of larger photo if motion detected early in scan
- 
-# Motion detection settings:
-# PGM maded changes to read values dynamically via command line parameters.
-# --------------------------
-# Threshold      - (how much a pixel has to change by to be marked as "changed")
-# Sensitivity    - (how many changed pixels before capturing an image) needs to be higher if noisy view
-# ForceCapture   - (whether to force an image to be captured every forceCaptureTime seconds)
-# filepath       - location of folder to save photos
-# filenamePrefix - string that prefixes the file name for easier identification of files.
-threshold = 10
-sensitivity = 180
-forceCapture = True
-forceCaptureTime = 60 * 60 # Once an hour
-filepath = "/home/pi/images/"
-filenamePrefix = "pgm"
-fileType = "jpg"
-
-# File photo size settings
-saveWidth = 800
-saveHeight = 600
-diskSpaceToReserve = 40 * 1024 * 1024 
-
-# Keep 40 mb free on disk
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
-#camera = picamera.PiCamera()
-
-# Email variables
-#user = 'mattwood262@gmail.com'
-#smtp_host = 'smtp.gmail.com'
-#smtp_port = 587
-# server = smtplib.SMTP()
-# server.connect(smtp_host,smtp_port)
-# server.ehlo()
-# server.starttls()
-# server.login(user,'')
-#fromaddr = 'matt@mntn.co.uk'
-#tolist = 'matt@mntn.co.uk'
-#sub = 'Subject: birdbox activity'</p><p># Capture a small test image (for motion detection)
+import time
 
 
-def captureTestImage():
-    imageData = StringIO()
-    # try:
-    camera.capture(imageData, format='bmp', use_video_port=True)
-    # command = "raspistill -w %s -h %s -t 1 -n -e bmp -o -" % (100, 75)
-    # except:
-    #    camera.capture(imageData, format='bmp', use_video_port=True)
-    imageData.seek(0)
-    im = Image.open(imageData)
-    buffer = im.load()
-    imageData.close()
-    return im, buffer
-    
-# Save a full size image to disk
+# uStream server info
+# uStream keys
+RTMP_URL="rtmp://1.21705518.fme.ustream.tv/ustreamVideo/21705518"
+STREAM_KEY="fKb9NJUycD2unefr9JhukXybZBRSB3Wq"
 
 
-def saveImage(width, height, diskSpaceToReserve):
-    keepDiskSpaceFree(diskSpaceToReserve)
-    time = datetime.now()
-    filename = filenamePrefix + "-%04d_%02d_%02d-%02d%02d%02d" % (time.year, time.month, time.day, time.hour, time.minute, time.second)+ "." + fileType
-    fullfilename = filepath + filename
-    # camera.resolution = (saveWidth, saveHeight)
-    camera.capture(fullfilename, format='jpeg', use_video_port=True)
-    # subprocess.call("echo %s > temp.out" % filename, shell=True)
-    # subprocess.call("/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload %s %s" % (fullfilename, filename), shell=True)
-    # msg = email.MIMEMultipart.MIMEMultipart()
-    # msg['From'] = fromaddr
-    # msg['To'] = tolist
-    # msg['Subject'] = sub  
-    # msg.attach(MIMEText('\nsent via python', 'plain'))
-    # server.sendmail(user,tolist,msg.as_string())
-    # print "Captured image: %s" % filename
-    camera.wait_recording(60)
-    
-# Keep free space above given level
-def keepDiskSpaceFree(bytesToReserve):
-    if (getFreeSpace() < bytesToReserve):
-        for filename in sorted(os.listdir(".")):
-            if filename.startswith(filenamePrefix) and filename.endswith("." + fileType):
-                os.remove(filename)
-                print ("Deleted %s to avoid filling disk") % filename
-                if (getFreeSpace() > bytesToReserve):
-                    return
+# ffmpeg command line
+cmdline =['ffmpeg', '-i' , '-', '-vcodec', 'copy', '-an', '-framerate', '25','-metadata', 'title=\"Ipswich IP1 bird box camera\"', '-f', 'flv', RTMP_URL +'/' + STREAM_KEY]
 
-# Get available disk space
-def getFreeSpace():
-    st = os.statvfs(".")
-    du = st.f_bavail * st.f_frsize
-    return du
-    
-#--------------------------------------------------------- 
-# Start capturing video
+# Setup up pipe to ffmpeg
+stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
+#Set up picamera
+camera = picamera.PiCamera()
 
+count = 0
 
-# camera.resolution = (1024, 768)
-
-with picamera.PiCamera() as camera: 
-	camera.resolution = (640, 480)
-	camera.framerate = 30
-	camera.start_recording(sys.stdout, format='h264')
-	camera.wait_recording(60)
+# Function to determine if camera is recording
+# If recording return False, is not recording return True
+def not_recording():
+	if camera.recording:
+		return False
+	else:
+		print("re-starting recording")
+		return True
+		
+#Function to start streaming for 30 minutes and then stop
+def stream_now():
+	print("About to start recording")
+	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
+	print("Record for 30 minutes")
+	camera.wait_recording(1800)
+	print("30 minutes expired, about to stop")
 	camera.stop_recording()
-	#camera.roatation =180
-	
-	
-# Get first image
-#image1, buffer1 = captureTestImage()
 
-# Reset last capture time
-#lastCapture = time.time()
+#Main code
+try:
+	camera.resolution = (960, 540)
+	camera.framerate = 25
+	camera.vflip = True
+	camera.hflip = True
+#Main loop
+# if not recording, start recording for 15 seconds
+	while True:
+		if not_recording():
+			count += 1
+			stream_now()
 
-# added this to give visual feedback of camera motion capture activity.  Can be removed as required
-#os.system('clear')
-#f = open('./logfile','w')
-#f.write('            Motion Detection Started')
-#f.write('            ------------------------')
+except KeyboardInterrupt:
+		camera.stop_recording()
 
-# print "Pixel Threshold (How much)   = " + str(threshold)
-# print "Sensitivity (changed Pixels) = " + str(sensitivity)
-# print "---------- Motion Capture File Activity --------------"</p><p>while (True):</p><p>    # Get comparison image
-#try: 
-#    image2, buffer2 = captureTestImage()
-#except:
-#    subprocess.call("echo 'timeout' > temp.out", shell=True)
-#    image2, buffer2 = captureTestImage()
-    # Count changed pixels
- #   changedPixels = 0
- #   for x in xrange(0, 100):
- #       # Scan one line of image then check sensitivity for movement
- #       for y in xrange(0, 75):
- #           # Just check green channel as it's the highest quality channel
- #           pixdiff = abs(buffer1[x,y][1] - buffer2[x,y][1])
- #           if pixdiff > threshold:
- #               changedPixels += 1
- #       # Changed logic - If movement sensitivity exceeded then
- #       # Save image and Exit before full image scan complete
- #       if changedPixels > sensitivity:   
- #           lastCapture = time.time()
- #           saveImage(saveWidth, saveHeight, diskSpaceToReserve)
- #           break
- #       continue
-    # Check force capture
- #   if forceCapture:
- #       if time.time() - lastCapture > forceCaptureTime:
- #           changedPixels = sensitivity + 1
-  
-    # Swap comparison buffers
- #   image1  = image2
- #   buffer1 = buffer2
-while True:
-	pass
-	
+finally:
+	camera.close()
+	stream.stdin.close()
+	stream.wait()
+	print("Camera safely shut down")
+	print("Good bye")
+	print("Count = ",count)

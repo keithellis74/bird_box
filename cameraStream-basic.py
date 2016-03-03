cdb52b4c95c2f818ca0fd98186d95cf4e3bb8d8e
Keith Ellis <keith.ellis@mac.com>
2016-03-03 23:17:29 +0000

    python script cameraStream-basic.py now streams video and takes snap shots at required intervals

--- cameraStream-basic.py
+++ cameraStream-basic.py
@@ -8,9 +8,9 @@
 
 import subprocess
 import picamera
-from signal import pause
-from time import sleep
+from time import sleep, time
 from datetime import datetime
+import errno
 
 # uStream server info
 # uStream keys
@@ -20,34 +20,37 @@ from datetime import datetime
 RTMP_URL="rtmp://1.22079439.fme.ustream.tv/ustreamVideo/22079439"
 STREAM_KEY="U8LmvKcPe3pCU5gbb5m2pexDXHCRBJDW"
 
+snapshot_frequency = 10  #frequency of snapshops in seconds
 
+max_images = 10 		# Maximum number of images to keep
+
+def stream_now():
+	print("About to start recording")
+	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
+	
+#Function to take a snapshop image	
+def snap_shot(name):
+	camera.capture('images/'+ name +'.jpg', use_video_port=True)  	
 
 # ffmpeg command line
 cmdline =['ffmpeg', 
 			'-i' , '-', 
 			'-vcodec', 'copy', 
 			'-an', 
-			#'-framerate', '25',
-			#'-maxrate', '500000',
-			#'-bufsize', '4000k',
-			#'-g', '50',
-			#'-timestamp', 'now',
 			'-metadata', 'title=\"Ipswich IP1 bird box camera\"', 
 			'-f', 'flv',
 			 RTMP_URL +'/' + STREAM_KEY]
 
+
 # Setup up pipe to ffmpeg
 stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
-def stream_now():
-	print("About to start recording")
-	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
-
+start= time()
+last = start
+snap_shot_count = 0
 
 #Set up picamera
 with picamera.PiCamera() as camera:
-		
-#Main loop
 	try:
 		camera.resolution = (960, 540)
 		camera.framerate = 25
@@ -60,9 +63,17 @@ with picamera.PiCamera() as camera:
 			i = datetime.now()
 			now = i.strftime('%d %b %Y - %H:%M:%S')
 			camera.annotate_text = ' Bird Cam - ' + now + ' '
-			camera.wait_recording(1)
-		#pause()
-			
+			camera.wait_recording(.2)
+
+			if time() - last > snapshot_frequency:  # Take snap shot
+				print("taking snap shot No.",snap_shot_count)
+				snap_shot('image'+str(snap_shot_count))
+				last = time()
+				if snap_shot_count >= max_images:
+					snap_shot_count = 0
+				else:
+					snap_shot_count += 1
+	
 	except KeyboardInterrupt:
 		camera.stop_recording() 
 	

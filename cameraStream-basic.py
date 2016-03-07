010c5006bb4bae636ee2efb64d8bb0daf3918c6c
Keith Ellis <keith.ellis@mac.com>
2016-03-07 21:31:15 +0000

    Work in progres update

--- cameraStream-basic.py
+++ cameraStream-basic.py
@@ -20,13 +20,24 @@ import errno
 RTMP_URL="rtmp://1.22079439.fme.ustream.tv/ustreamVideo/22079439"
 STREAM_KEY="U8LmvKcPe3pCU5gbb5m2pexDXHCRBJDW"
 
-snapshot_frequency = 10  #frequency of snapshops in seconds
+snapshot_frequency = 60 * 60  #frequency of snapshops in seconds
 
-max_images = 10 		# Maximum number of images to keep
+max_images = 24 		# Maximum number of images to keep
 
-def stream_now():
-	print("About to start recording")
-	camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
+def stream_now(val):
+	global stream
+	global cmdline
+	print("Camera has been closed", camera.closed)
+	print("Camera is recording", camera.recording)
+	if val == "restart":
+		print("re-opening pipe")
+		stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+		sleep(1)
+		print("About to start recording")
+		camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
+	if val == "start":
+		print("About to start recording")
+		camera.start_recording(stream.stdin, format='h264', bitrate = 500000)
 	
 #Function to take a snapshop image	
 def snap_shot(name):
@@ -43,7 +54,10 @@ cmdline =['ffmpeg',
 
 
 # Setup up pipe to ffmpeg
+
+print("Setup stream")
 stream = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+sleep(1)
 
 start= time()
 last = start
@@ -51,20 +65,20 @@ snap_shot_count = 0
 
 #Set up picamera
 with picamera.PiCamera() as camera:
-	try:
-		camera.resolution = (960, 540)
-		camera.framerate = 25
-		camera.vflip = True
-		camera.hflip = True
-		camera.annotate_background = picamera.Color('black')
-		while True:
+	camera.resolution = (960, 540)
+	camera.framerate = 25
+	camera.vflip = True
+	camera.hflip = True
+	camera.annotate_background = picamera.Color('black')
+	while True:
+		try:
 			if not camera.recording:
-				stream_now()
+				stream_now("start")
 			i = datetime.now()
 			now = i.strftime('%d %b %Y - %H:%M:%S')
 			camera.annotate_text = ' Bird Cam - ' + now + ' '
 			camera.wait_recording(.2)
-
+	
 			if time() - last > snapshot_frequency:  # Take snap shot
 				print("taking snap shot No.",snap_shot_count)
 				snap_shot('image'+str(snap_shot_count))
@@ -74,12 +88,17 @@ with picamera.PiCamera() as camera:
 				else:
 					snap_shot_count += 1
 	
-	except KeyboardInterrupt:
-		camera.stop_recording() 
+		except IOError as e:
+			if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+				stream.kill()
+				stream_now("restart")
+
+#	except KeyboardInterrupt:
+#		camera.stop_recording() 
 	
-	finally:
-		stream.stdin.close()
-		stream.wait()
-		print("Camera safely shut down")
-		print("Good bye")
+#		finally:
+#			stream.stdin.close()
+#			stream.wait()
+#			print("Camera safely shut down")
+#			print("Good bye")
 	

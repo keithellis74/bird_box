42ab0e1c02eb4bab1fb93a9ee97c73834ff9d3eb
Keith Ellis <keith.ellis@mac.com>
2016-04-02 12:50:53 +0100

    Edited thingspeak.py library added and temperature is now overlaided

--- cameraStreamBash1.py
+++ cameraStreamBash1.py
@@ -13,21 +13,36 @@ import sys
 from signal import pause
 from time import sleep, time
 from datetime import datetime
+import thingspeak
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
 
 snapshot_frequency = 60 * 60  #frequency of snapshops in seconds
-
+temp_frequency = 5 * 60  #frequency of temperature updates
 max_images = 24 		# Maximum number of images to keep
+
+#ThingSpeak keys
+channel_id = "88282"
+write_key  = "LE6B6L2ZRZ592AA8"
+channel = thingspeak.Channel(id=channel_id,write_key=write_key)
+
+
+def get_temp(sensor):
+	if sensor == 'internal':
+		field_id = 2
+	else:
+		field_id = 1
+	return channel.get_field_last_text(field=field_id)
 
 
 def start_stream():
 	start= time()
 	last = start
 	snap_shot_count = 0
+	ext_temp = get_temp('external')
 
-	
-#		print("Create camera object")
+
+#	print("Create camera object")
 	camera = picamera.PiCamera()
 	camera.resolution = (960, 540)
 	camera.framerate = 25
@@ -40,9 +55,13 @@ def start_stream():
 	camera.start_recording(sys.stdout, bitrate = 500000, format='h264')
 	
 	while True:
+		
+		if time()- last > temp_frequency:
+			ext_temp = get_temp('external')
+
 		i = datetime.now()
 		now = i.strftime('%d %b %Y - %H:%M:%S')
-		camera.annotate_text = ' Bird Cam - ' + now + ' '
+		camera.annotate_text = ' Bird Cam - ' + now + ' ' + ' Ext temp = ' + ext_temp
 		
 		try:
 			camera.wait_recording(0.2)

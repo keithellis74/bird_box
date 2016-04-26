be9373d4fd645d32f561aa440f8cbd8cbe194032
Keith Ellis <keith.ellis@mac.com>
2016-04-26 23:24:08 +0100

    add last_snap and last_temp variables

--- cameraStreamBash1.py
+++ cameraStreamBash1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # This Python script sends picamera video output to stout,
-# it is intended to be called from a bash script which pipes 
+# it is intended to be called from a bash script which pipes
 # the output into ffmpeg
 #
 # Bash script is startPythonStream.sh
@@ -18,7 +18,7 @@ import thingspeak
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
 
 snapshot_frequency = 60 * 60  #frequency of snapshops in seconds
-temp_frequency = 5 * 60  #frequency of temperature updates
+temp_frequency = 2 * 60  #frequency of temperature updates
 max_images = 24 		# Maximum number of images to keep
 
 #ThingSpeak keys
@@ -39,58 +39,51 @@ def get_temp(sensor):
 
 def start_stream():
 	start= time()
-	last = start
+	last_temp, last_snap = start
 	snap_shot_count = 0
 	ext_temp = get_temp('external')
-
-#	print("Create camera object")
 	camera = picamera.PiCamera()
-	camera.roatation = 90
 	camera.resolution = (960, 540)
 	camera.framerate = 25
 	camera.vflip = True
 	camera.hflip = True
 	camera.annotate_background = picamera.Color('black')
 
-	
-#	print("About to start recording")
 	camera.start_recording(sys.stdout, bitrate = 500000, format='h264')
-	
-	while True:
-		
-		if time()- last > temp_frequency:
-			ext_temp = get_temp('external')
 
+	while True:
+		if time()- last_temp > temp_frequency:
+			ext_temp = get_temp('external')
+			last_temp = time()
 		i = datetime.now()
 		now = i.strftime('%d %b %Y - %H:%M:%S')
-		camera.annotate_text = ' Bird Cam - ' + now + ' -' + ' Ext temp = ' + ext_temp + 'C'+ ' '
-		
+		camera.annotate_text = (' Bird Cam - {0} - Ext temp = {1}C '
+								.format(now, ext_temp))
+
 		try:
 			camera.wait_recording(0.2)
-		
+
 		except picamera.exc.PiCameraError:
 			camera.close()
-			break			
-			
+			break
+
 		except BrokenPipeError:
 			camera.close()
 			break
-			
-		if time() - last > snapshot_frequency:  # Take snap shot
-#			print("taking snap shot No.",snap_shot_count)
+
+		if time() - last_snap > snapshot_frequency:  # Take snap shot
 			snap_shot('image'+str(snap_shot_count), camera)
-			last = time()
+			last_snap = time()
 			if snap_shot_count >= max_images:
 				snap_shot_count = 0
 			else:
 				snap_shot_count += 1
-	
-				
-	
-#Function to take a snapshop image	
+
+
+
+#Function to take a snapshop image
 def snap_shot(name, camera):
-#	global camera
-	camera.capture('images/'+ name +'.jpg', use_video_port=True)  	
+	camera.capture('images/'+ name +'.jpg', use_video_port=True)
 
 
 start_stream()
